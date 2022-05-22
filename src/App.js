0b731c4270c5d393ede6e6d6ca998960f43011ca
git-mhasan <mahadi2020dev@gmail.com>
2022-05-22 14:27:14 +0600

    404 page ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Header from './Shared/Header/Header';
+import FourOfour from './Pages/FourOfour/FourOfour';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header logo={logo}></Header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/*' element={<FourOfour />} />
       </Routes>
     </div>
   );
