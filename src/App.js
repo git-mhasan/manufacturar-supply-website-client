701fff7401973e2c440db306326403fd9fffdb8a
git-mhasan <mahadi2020dev@gmail.com>
2022-05-24 08:55:01 +0600

    login form with react-hoohk -form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login'
 import Header from './Shared/Header/Header';
 import FourOfour from './Pages/FourOfour/FourOfour';
 
@@ -11,6 +12,7 @@ function App() {
       <Header logo={logo}></Header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path='/*' element={<FourOfour />} />
       </Routes>
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const { register, handleSubmit, formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
+
     return (
         <div>
-            Login Form
+
+
+            <div className="hero min-h-full ">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border-t-2">
+                        <div className="card-body">
+                            <h1 className="text-2xl font-bold text-center">Login</h1>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+
+                                {/* email  */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input
+                                        type="email"
+                                        placeholder="Your Email"
+                                        className="input input-bordered w-full max-w-xs"
+                                        {...register("email", {
+                                            required: {
+                                                value: true,
+                                                message: 'Email is Required'
+                                            },
+                                            pattern: {
+                                                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                                message: 'Provide a valid Email'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                    </label>
+                                </div>
+
+                                {/* password */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input
+                                        type="password"
+                                        placeholder="Password"
+                                        className="input input-bordered w-full max-w-xs"
+                                        {...register("password", {
+                                            required: {
+                                                value: true,
+                                                message: 'Password is Required'
+                                            },
+                                            minLength: {
+                                                value: 6,
+                                                message: 'Must be 6 characters or longer'
+                                            }
+                                        })}
+                                    />
+                                    <label className="label">
+                                        {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                        {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                    </label>
+
+                                    {/* Forget Password */}
+                                    <label className="label">
+                                        <Link to='' className="label-text-alt link link-hover">Forgot password?</Link>
+                                    </label>
+                                </div>
+
+                                {/* submit button */}
+                                <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+
+                            </form>
+
+                        </div>
+                    </div>
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
